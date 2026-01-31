638cebcba25cdaab3528a2537f3865198919a20c
Obed Darko <darkoobed555555@gmail.com>
2026-01-31 05:38:03 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+         // Smooth scroling
+         document.querySelectorAll('a[href^="#"]').forEach
+         (anchor=> {
+           anchor.addEventListener('click', function(e) {
+             e.preventDefault();
+             document.querySelector(this.getAtrribute
+             ('href')).scrollintoView({
+               behavior: 'Smooth'
+             });
+          });
+       });
+       
+       // Navbar scroll effect
+       window.addEventListener('scroll', () => {
+         const navbar = document.querySelector('.navbar');
+         window.scrollY > 50 ?
+            navbar.style.backgroundColor = 'rgba(10,10,10,.098)' :
+            navbar.style.backgroundColor = 'rgba(10,10,10,0.95)';
+       });
